effb43908874862087856718a8e8b78e46515c46
Shawn Hope <shawnelmohope@gmail.com>
2017-08-28 12:00:21 -0600

    Unity Animal CLASSES

---
+++ 1600Fall2017/Assets/Animal.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animal : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+			Die();
+	}
+	
+	void Die () {
+		print(this.name + " Died");
+	}	
+	
+	
+		
+	}
+
---
+++ 1600Fall2017/Assets/Fish.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fish : Animal {
+
+    void Swim () {
+    print(this.name + " swims");
+    }
+
+    void Start(){
+    Swim ();
+    }
+
+}
