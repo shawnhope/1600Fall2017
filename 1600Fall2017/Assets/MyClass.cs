e5fb455c680c201504815dfbf080dcd634c0111e
Shawn Hope <shawnelmohope@gmail.com>
2017-09-06 11:52:45 -0600

    Syntax, Functions (Methods) and Parameters (Variables)

---
+++ 1600Fall2017/Assets/MyClass.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyClass : MonoBehaviour {
+
+	string myString = "Hello";
+
+	void Start () {
+		print(myString);
+		}				
+	
+	void Update () {
+		
+	}
+}
