f8447138cee9d7f62d4276376fa5c3884f5cb91e
Shawn Hope <shawnelmohope@gmail.com>
2017-09-13 11:44:57 -0600

    Added MathOperators and ToggleButton

--- 1600Fall2017/Assets/ifStatement.cs
+++ 1600Fall2017/Assets/ifStatement.cs
@@ -6,12 +6,25 @@ using UnityEngine.UI;
 public class ifStatement : MonoBehaviour {
 
 public Text input;
+public bool canPlayGame = false;
 public string password = "dank";
 
 void Update(){
 	if(input.text == password){
 		print("YOo youre right i guess");
+		canPlayGame = true;
 		}
+		else{
+			print("invaild password, maybe you should've wrote it down");
+		}
+
+		if(canPlayGame){
+			print("Playing game.");
+		}
+		else{
+			print("Can't play yet, Enter correct password");
+		}
+
 	}	
 
 }
