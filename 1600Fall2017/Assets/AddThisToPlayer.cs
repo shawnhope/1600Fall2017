a1595bc724ae4f1ee0114a0b50952e42a6c0c8bd
Shawn Hope <shawnelmohope@gmail.com>
2017-09-25 11:59:06 -0600

    lists

---
+++ 1600Fall2017/Assets/AddThisToPlayer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddThisToPlayer : MonoBehaviour {
+
+	public Transform attachObject;
+	public GameObject assetArt;
+	void OnTriggerEnter()
+	{
+		transform.parent = attachObject;
+		transform.localPosition = Vector3.zero;
+		transform.localRotation = Quaternion.identity;
+
+	}
+}
---
+++ 1600Fall2017/Assets/Litsts.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Litsts : MonoBehaviour {
+	public List<GameObject> myWeapons;
+
+void onTriggerEnter(Collider _weapon)
+{
+	myWeapons.Add(_weapon.gameObject);
+}
+
+
+}
