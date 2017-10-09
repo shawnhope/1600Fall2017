db35af1c6a69b91d06cd90a9a44b9c2f9e5e6bbe
Shawn Hope <shawnelmohope@gmail.com>
2017-10-09 11:34:47 -0600

    In-Class MathFunctions

---
+++ 1600Fall2017/Assets/Scripts/MathFunctions.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MathFunctions : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		myScore = ReturnNum(health, ammo);
+		myPassword = ReturnPassword(myPassword);
+	}
+	
+	public int myScore;
+
+	public int health;
+	public int ammo;
+
+	public string myPassword = "CoolStuff";
+
+	int ReturnNum(int _num, int _num2) {
+		return _num + _num2;
+	}
+
+	string ReturnPassword(string _login){
+		if(_login == "ou812"){
+			return "Password Correct";
+		}
+		else{
+			return "Incorrect";
+		}
+	}
+
+}
