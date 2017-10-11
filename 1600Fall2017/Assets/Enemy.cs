240d07b8c7d9ec373355427791ecdc64aca32b52
Shawn Hope <shawnelmohope@gmail.com>
2017-10-11 11:55:19 -0600

    NavMeshAgent

---
+++ 1600Fall2017/Assets/Enemy.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy : MonoBehaviour {
+
+	public NavMeshAgent agent;
+	public Transform player;
+
+	void Update () {
+		agent.destination = player.position;
+	}
+
+	public void Stun (float _speed){
+		agent.speed = _speed;
+
+	}
+
+}
---
+++ 1600Fall2017/Assets/practice stuff/movement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour {
+
+public float speed = 3f;
+public float height = 4f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey(KeyCode.D))
+		transform.position += transform.right * Time.deltaTime * height;
+			//transform.Translate(speed * Time.deltaTime, 0, 0);
+		else if(Input.GetKey(KeyCode.A))
+		transform.position -= transform.right * Time.deltaTime * height;
+			//transform.Translate(-speed * Time.deltaTime, 0, 0);
+		else if(Input.GetKey(KeyCode.W))
+			transform.position += transform.forward * Time.deltaTime * height;
+		//transform.Translate(0,height * Time.deltaTime, 0);
+		else if(Input.GetKey(KeyCode.S))
+		transform.position -= transform.forward * Time.deltaTime * height;
+			//transform.Translate(0, -height * Time.deltaTime, 0);
+	}
+}
