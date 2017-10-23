9f6db07f55e59519a73e679126fa9f4cba951c46
Shawn Hope <shawnelmohope@gmail.com>
2017-10-23 11:55:51 -0600

    HP UI bar, hp up and down powerup

---
+++ 1600Fall2017/Assets/Scripts 1/UI/UIBar.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using UI
+using UnityEngine.UI;
+
+public class UIBar : MonoBehaviour {
+
+public Image bar;
+public float barTime = 0.1f;
+
+	void OnTriggerEnter(){
+		//starts the coroutine
+		StartCoroutine(UpdateBar());
+	}
+	
+	//a coroutine..ok?
+	IEnumerator UpdateBar (){
+		while(bar.fillAmount < 1){
+			bar.fillAmount += barTime;
+			//takes one barTime then waits second
+			yield return new WaitForSeconds(barTime);
+		}
+	}
+}
