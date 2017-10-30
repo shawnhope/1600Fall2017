0e71a8d001079f71945c34be721efa09978948ff
Shawn Hope <shawnelmohope@gmail.com>
2017-10-30 11:54:32 -0600

    InClass ReplayButton and Checkpoints

---
+++ 1600Fall2017/Assets/Scripts 1/Character/CheckPoint.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckPoint : MonoBehaviour {
+
+	void OnTriggerEnter()
+	{
+		ReplayGame.startPosition = transform.position;
+	}
+}
---
+++ 1600Fall2017/Assets/Scripts 1/UI/ReplayGame.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ReplayGame : MonoBehaviour {
+
+	public Transform player;
+	public Image uiBar;
+	public GameObject GameOverUI;
+	public static Vector3 startPosition;
+	private float fillAmount;
+
+
+	void Awake()
+	{
+		startPosition = player.position;
+		fillAmount = uiBar.fillAmount;
+		GameOverUI.SetActive(false);
+	}
+
+
+	public void Click(){
+		CharacterControl.gameOver = false;
+		player.position = startPosition;
+		uiBar.fillAmount = fillAmount;
+		GameOverUI.SetActive(false);
+	}
+
+}
