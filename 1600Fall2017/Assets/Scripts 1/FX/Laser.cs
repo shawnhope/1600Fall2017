cfae3904fabea1f884fa61b3e93491aa47d6c790
Shawn Hope <shawnelmohope@gmail.com>
2017-11-10 11:51:31 -0700

    InClass LaserParticle demo

---
+++ 1600Fall2017/Assets/Scripts 1/FX/Laser.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour {
+
+public Transform start;
+public Transform end;
+public LineRenderer line;
+
+
+	void Update()
+	{
+		line.SetPosition(0, start.position);
+		line.SetPosition(1, end.position);	
+	}
+}
