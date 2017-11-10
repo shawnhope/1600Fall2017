afae7d57aa409a385558f41878d9f7faebe4ecae
Shawn Hope <shawnelmohope@gmail.com>
2017-11-10 09:09:20 -0700

    Proj2 001 main
    i can't remember what i need for a script so gotta switch back to inclass branch :/

---
+++ PROJECT2/PROJECT2/Assets/characterControl.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class characterControl : MonoBehaviour {
+
+	public CharacterControl;
+	public float gravity = 9.81f, speed, jumpForce;
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
