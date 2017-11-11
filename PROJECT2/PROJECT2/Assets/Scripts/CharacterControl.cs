39dc0148de2942f238bd50a8fb885f00facdc4ca
Shawn Hope <shawnelmohope@gmail.com>
2017-11-11 16:48:03 -0700

    Project - Side to Side and Jumping
    Take-2 (more like 5 because saving issues smh).

---
+++ PROJECT2/PROJECT2/Assets/Scripts/CharacterControl.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterControl : MonoBehaviour {
+
+	public CharacterController characterControl;
+	public float speed = 15f, gravity = 9.81f, jumpForce = 50f;
+	public Vector3 moveVector3;
+
+	void Update () {
+		if (!characterControl.isGrounded) {
+			//when not grounded, V3.y = (downward)gravity
+			moveVector3.y -= gravity * Time.deltaTime;
+		}
+		if (characterControl.isGrounded) {
+			if (Input.GetKeyDown (KeyCode.Space)) {
+				moveVector3.y = jumpForce * Time.deltaTime;
+			}
+			moveVector3.x = Input.GetAxis ("Horizontal") * speed * Time.deltaTime;
+		} 
+		characterControl.Move (moveVector3);
+	}
+}
