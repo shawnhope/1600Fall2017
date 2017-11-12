13936db7ada3973888b207e948e6a47b3cfb2742
Shawn Hope <shawnelmohope@gmail.com>
2017-11-11 21:11:11 -0700

    Project - Camera Experiments
    003; success!?

---
+++ PROJECT2/PROJECT2/Assets/Scripts/CameraRot.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRot : MonoBehaviour {
+
+	public GameObject camTarget;
+	public float rotSpeed = 150f;
+	public Vector3 camTargetRot;
+
+	void Update () {
+		camTargetRot.y += Input.GetAxis ("Mouse X") * rotSpeed * Time.deltaTime;
+		camTargetRot.x -= Input.GetAxis ("Mouse Y") * rotSpeed * Time.deltaTime;
+		camTarget.transform.eulerAngles = camTargetRot;
+		//to make delay/lagged cam, have it 'chase' player and a little slower speed than player?
+	}
+}
