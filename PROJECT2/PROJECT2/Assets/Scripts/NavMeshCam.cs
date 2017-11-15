fea80cf66f68078da4faf9bcc2862d711e6b458b
Shawn Hope <shawnelmohope@gmail.com>
2017-11-15 11:48:21 -0700

    Project - Added NavMesh Camera
    003 Added NavMesh Cam

---
+++ PROJECT2/PROJECT2/Assets/Scripts/NavMeshCam.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavMeshCam : MonoBehaviour {
+
+public Transform player;
+public NavMeshAgent agent;
+	
+	// Update is called once per frame
+	void Update () {
+		agent.destination = player.position;
+	}
+}
