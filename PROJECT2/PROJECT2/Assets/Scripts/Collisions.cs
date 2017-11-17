b5b8fd55085e1b5037e5da3f9fa3ab37bbedb94d
Shawn Hope <shawnelmohope@gmail.com>
2017-11-17 12:01:58 -0700

    InClass Project block colliding?
    send to teach if still doesn't work on surf

---
+++ PROJECT2/PROJECT2/Assets/Scripts/Collisions.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collisions : MonoBehaviour {
+
+public GameObject RedMush;
+public Transform spawnpoint;
+public enum CollisionThing{
+	Block
+}
+public CollisionThing colThing;
+
+	void  OnCollisionEnter()
+	{
+		switch(colThing){
+			case CollisionThing.Block:
+			Instantiate(RedMush, spawnpoint);
+			break;
+		}
+	}
+}
