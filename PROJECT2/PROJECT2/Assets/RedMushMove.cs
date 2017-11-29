49b25539b864e75b3761e09d76450c417304f444
Shawn Hope <shawnelmohope@gmail.com>
2017-11-29 11:51:28 -0700

    InClass Project - item movement idea
    didn't work

---
+++ PROJECT2/PROJECT2/Assets/RedMushMove.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RedMushMove : MonoBehaviour {
+
+public Vector3 mushMove;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		mushMove.x = 3*Time.deltaTime;
+		transform.position = mushMove;
+	}
+}
