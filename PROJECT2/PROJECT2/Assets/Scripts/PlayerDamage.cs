1ebdee0ad133e42ace7910561edada0b452fa6a0
Shawn Hope <shawnelmohope@gmail.com>
2017-12-06 08:07:28 -0700

    Project - can't figure out arrays..
    012; can't figure out how to swap gameobj array.... So just going to have to remember to delete all the crap i made and go back to how I had it. It'll have the same visual effect either way :T

---
+++ PROJECT2/PROJECT2/Assets/Scripts/PlayerDamage.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDamage : MonoBehaviour {
+
+	public GameObject[] player;
+	public Transform playerPos;
+
+	void Update(){
+		if (Powers.twranched == true) {
+			player [0].transform.position = playerPos.position;
+		} 
+		else if (Powers.twranched == false) {
+			player [1].transform.position = playerPos.position;
+		}
+	}
+	//way to swap between array
+	//way to trigger the swap
+
+}
