e7147b0fa246c6d66a34d3a1d8906dd7965d0c3d
Shawn Hope <shawnelmohope@gmail.com>
2017-12-13 02:54:41 -0700

    Project - Proceeding to admit defeat
    so screwed

---
+++ PROJECT2/PROJECT2/Assets/Scripts/BulletAgent.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class BulletAgent : MonoBehaviour {
+
+	public NavMeshAgent billAgent;
+	public Transform target, spawn;
+	public GameObject bill;
+
+
+	void Update ()
+	{
+		billAgent.destination = target.position;
+		if (CharacterControl.gameOver == true) {
+			bill.transform.position = spawn.transform.position;
+		} 
+	}
+}
+
